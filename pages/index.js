abd553df6f072b1af5e2ad214d1d6bf119c8bc00
Vinicius Finkler <viniciusaf.99@gmail.com>
2025-12-15 14:03:13 -0300

    Alterando layout da pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,104 +1,235 @@
 import { useState } from "react";
 
 function Home() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpen = () => {
+      console.log('open')
+      setIsModalOpen(true);
+    };
+
+    const handleClose = () => {
+      setIsModalOpen(false);
+    };
+
     return <div className="container">
                 <div className="header">
-                  <h1>Você foi escolhido como cobaia do meu novo projeto.</h1>
-                  <h2>Então dá play no vídeo e aproveita pra dançar ao som de MC Andinho:</h2>
+                  <h1>Olá!</h1>
+                  <h2>Vamos tomar um café essa semana?</h2>
+                  <div className="content">
+                    <div className="declineWrapper">
+                      <button className="decline">NÃO</button>
+                    </div>
+                    <button className="accept" onClick={handleOpen}>SIM</button>
+                  </div>
                 </div>
-                <div className="body">
-                    <iframe width="350" height="200" src="https://www.youtube.com/embed/fOYHfvREq_8?si=tsUHEJjFKYkT0nIZ?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>                
-                    <img src="https://25.media.tumblr.com/tumblr_maiks37LUo1qj9zwno1_250.gif" alt="Gato dançante"/>
+                <div className={`${isModalOpen ? 'modalOpen' : 'modal'}`}>
+                    <div className="modalWrapper">
+                      <div className="modalAction">
+                        <button className="closeModalButton" onClick={handleClose}>X</button>
+                      </div>
+                      <div className="modalHeader">
+                        <span>Excelente escolha!<br/>Me mande um 🧡 para confirmar!<br/></span>
+                      </div>
+                      <div className="modalContent">
+                        <img src="https://i.pinimg.com/originals/e9/26/16/e9261611196ebd98b2d76ab0627699a0.gif" alt="Café GIF"/>
+                      </div>
+                    </div>
                 </div>
         <style jsx>{`
-        .container {
-          display: flex;
-          flex-direction: column;
-          gap: 64px;
-          background-image: url('https://i.pinimg.com/originals/34/ac/31/34ac31c2ab7103586c407ec93ee15e7d.gif');
-          background-size: cover; 
-          height: 100vh;
-          overflow: hidden;
-          padding: 16px;
-          margin:0;
-
-          .header {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            h1 {
-                font-size: 50px;
-                background-color: black;
-                color: white;
-                margin: 8px;
-            }
-            h2 {
-                font-size: 36px;
-                background-color: black;
-                color: white;
-                margin: 8px;
-            }
-          }
-          .body {
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            align-items: center;
-            gap: 24px;
-
-            img {
-                height: 400px;
-                width: 200px;
-            }
-          }
-        }
-        @media screen and (max-width: 600px) {
           .container {
-          display: flex;
-          flex-direction: column;
-          gap: 24px;
-          background-image: url('https://i.pinimg.com/originals/34/ac/31/34ac31c2ab7103586c407ec93ee15e7d.gif');
-          background-size: cover; 
-          height: 100vh;
-          overflow: auto;
-          padding: 16px;
-          margin:0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background-image: url('https://cdn.wallpapersafari.com/56/63/9nlGN0.gif');
+            background-size: contain; 
+            height: 100vh;
+
+            button {
+              font-family: inherit;
+            }
 
           .header {
             display: flex;
             flex-direction: column;
             justify-content: center;
             align-items: center;
+            background-color: #e87c58;
+            border-radius: 10px;
+            border-style: solid;
+            border-color: white;
+            padding: 16px;
+            margin-bottom: 24px;
             h1 {
                 font-size: 32px;
-                background-color: black;
                 color: white;
                 margin: 8px;
+                padding: 8px;
             }
+
             h2 {
-                font-size: 24px;
-                background-color: black;
-                color: white;
-                margin: 8px;
+              font-size: 24px;
+              color: white;
+              margin-top: 0;
+              padding: 8px;
             }
           }
-          .body {
+
+          .content {
             display: flex;
-            flex-direction: column;
             justify-content: center;
             align-items: center;
-            gap: 24px;
+            gap: 40px;
 
-            img {
-                height: 200px;
+            button {
+              border-radius: 5px; 
+
+              &.decline {
+                height: 50px;
+                width: 200px;
+                background-color: #964a53;
+                border-style: solid;
+                color: white;
+                border-color: white;
+              }
+
+              &.accept {
+                transition: 1s;
+                height: 50px;
+                width: 200px;
+                background-color: #4a965e;
+                border-style: solid;
+                color: white;
+                border-color: white;
+              }
+            } 
+          }
+
+          .modal {
+            display: none;
+          }
+
+          .modalOpen {
+            position: absolute;
+            font-size: 60px;
+
+            .modalWrapper {
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              border-style: solid;
+              border-color: white;
+              border-radius: 10px;
+              height: 300px;
+              width: 500px;
+              background-color: #e87c58;
+
+              .modalAction {
+                display: flex;
+                align-self: end;
+                margin: 8px;  
+
+                 .closeModalButton {
+                  color: black;
+                  background-color: white;
+                  border-radius: 10px;
+                  border-style: solid;
+                  border-color: black;
+                  cursor: pointer;
+                }
+
+                .closeModalButton:hover {
+                  background-color: gray;
+                }
+              }
+
+              .modalHeader {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                text-align: center;
+              }
+
+              span {
+                color: white;
+                font-size: 24px;
+                padding: 24px;
+              }
+            }
+
+            .modalContent {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              gap: 32px;
+
+              img {
+                height: 100px;
                 width: 100px;
-                margin-bottom: 16px;
+              }
             }
           }
+
+          .declineWrapper:hover {
+            button {
+              visibility: hidden;
+            }
+          }
+
+          .decline:hover {
+            background-color: #5c1d24 !important;
+          }
+
+          .accept:hover {
+            cursor: pointer;  
+            background-color: #143d13 !important;
+          }
         }
+
+        @media screen and (max-width: 600px) {
+          .container {
+            background-size: cover;
+            background-image: url('https://i.pinimg.com/originals/0a/61/d7/0a61d7378ef0b113b833f05ec17178ba.gif');
+          }
+
+          .header {
+            h1 {
+              font-size: 24px !important;
+            }
+
+            h2 {
+              font-size: 18px !important;
+            }
+          }
+
+          .content {
+            button {
+              width: 150px !important;
+            }
+            gap: 0 !important;
+
+            .decline {
+              display: none;
+            }
+
+            .accept {
+              flex: 1;
+            }
+          }
+
+          .modalWrapper {
+              height: 300px !important;
+              width: 95vw !important;
+
+              span {
+                font-size: 12px;
+                padding: 24px;
+              }
+            }
         }
-      `}</style>
+      `}  </style>
     </div>;
     
 }
