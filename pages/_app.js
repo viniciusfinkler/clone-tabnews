1d1679d62fb267b45b05faa1ea2295ec8c3027ab
Vinicius Finkler <viniciusaf.99@gmail.com>
2025-12-18 12:01:50 -0300

    instala jest e configura scprits de teste

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import "../styles/globals.css";
 import { Crimson_Text } from "@next/font/google";
 
-const cabinSketch = Crimson_Text({ subsets: ["latin"], weight: ["400"] });
+const crimsonText = Crimson_Text({ subsets: ["latin"], weight: ["400"] });
 
 export default function App({ Component, pageProps }) {
   return (
-    <main className={cabinSketch.className}>
+    <main className={crimsonText.className}>
       <Component {...pageProps} />;
     </main>
   );
