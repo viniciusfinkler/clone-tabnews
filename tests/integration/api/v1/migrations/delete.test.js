cf201f9f93809642fcab9c2efaff93f869a1d9c8
Vinicius Finkler <viniciusaf.99@gmail.com>
2026-01-19 13:12:32 -0300

    makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- tests/integration/api/v1/migrations/delete.test.js
+++
@@ -1,6 +0,0 @@
-test("DELETE to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
-  });
-  expect(response.status).toBe(405);
-});
